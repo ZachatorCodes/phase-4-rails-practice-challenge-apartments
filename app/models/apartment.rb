586db14ade72f2d13d6f9702f3ddfee5655d3370
Zach Schwartz <zachatorcodes@gmail.com>
2023-09-05 13:53:20 -0600

    Added validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,2 +1,6 @@
 class Apartment < ApplicationRecord
+  has_many :leases
+  has_many :tenants, through: :leases
+
+  validates :number, numericality: { only_integer: true, message: "Apartment number must be an integer" }
 end
--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,7 @@
 class Tenant < ApplicationRecord
+  has_many :leases
+  has_many :apartments, through: :leases
+
+  validates :name, presence: true
+  validates :age, numericality: { greater_than_or_equal_to: 18, message: "Must be 18 years or older"}
 end
